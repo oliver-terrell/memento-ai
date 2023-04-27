548b57bbc9f846383777c1e5d146b64be9eb3584
Oliver T <oterrell28@gmail.com>
2023-04-27 11:36:10 -0500

    styling and error handling

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,25 @@ import { useState } from "react";
 import styles from "./index.module.css";
 import axios from "axios";
 
+
+const devData = {data: 
+"outgoing:0.8960937095330063,"+
+"intellectual:0.7939263435966878,"+
+"sympathy:0.7783130212148792,"+
+"cooperative:0.7511118017478042,"+
+"dutiful:0.7486107102951731,"+
+"trusting:0.7467607905816888,"+
+"authority_challenging:0.742335782627034,"+
+"uncompromising:0.7197297637282543,"+
+"modesty:0.7196483672608609,"+
+"emotionally_aware:0.6747619153328746,"+
+"gregariousness:0.6745473246461103,"+
+"cheerful:0.6639879829215412,"
+};
+
+
+
+
 export default function Home() {
   const [query, setQuery] = useState("");
   const [prevQuery, setPrevQuery] = useState(query);
@@ -20,9 +39,11 @@ export default function Home() {
     }
 
     setLoading(true);
+    setError(false);
 
     try {
       const response = await axios.post("/api/bigFive", { query: query });
+      // const response = devData;
       var data = JSON.stringify(response.data).split(',');
       var output = data.filter(trait => trait.includes('.'))
         .sort((a, b) => {
