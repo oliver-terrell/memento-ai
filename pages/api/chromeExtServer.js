3bdafdbf51dba01866d8df66d0aac02a82edd5a4
Oliver T <oterrell28@gmail.com>
2023-05-23 22:13:02 -0500

    no res just ret

--- pages/api/chromeExtServer.js
+++ pages/api/chromeExtServer.js
@@ -58,20 +58,14 @@ async function getTextSummary(visibleText, bigFiveData) {
       temperature: 0.8,
       max_tokens: 256,
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
+    return completion.data.choices[0].text;
   } catch(error) {
-    // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
       console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
+      return {
         error: {
           message: 'An error occurred during your request.',
         }
-      });
-    }
+    };
   }
 }
 
